f7310ef715efeb0d0bbc39eadb61b338cacfbd6f
NikhilOli <nikhiloli107@gmail.com>
2024-04-27 20:41:41 +0545

    Project setup

---
+++ usersData-mern/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='flex items-center justify-center mx-auto text-indigo-800'>MERN APP</h1>
+      <h3>Hello</h3>
+    </>
+  )
+}
+
+export default App
