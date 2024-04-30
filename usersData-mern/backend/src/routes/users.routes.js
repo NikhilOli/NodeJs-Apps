7b3b3eff708de15a9347e43f1d72f762e3d2ad3d
NikhilOli <nikhiloli107@gmail.com>
2024-04-30 12:30:39 +0545

    update and delete route ✔️✅

--- usersData-mern/backend/src/routes/users.routes.js
+++ usersData-mern/backend/src/routes/users.routes.js
@@ -27,11 +27,44 @@ router.post("/addUser", async (req, res) => {
 router.get("/showUsers", async (req, res) => {
     try {
         const allUsers = await User.find();
-        res.status(200).json(allUsers);
+        let userHTML = "<h1>All Users</h1>";
+        allUsers.forEach(user => {
+            userHTML += `<li>${user.name}</li>`;
+        });
+        res.status(200).send(userHTML);
     } catch (error) {
         console.log("Error in fetching user", error);
 
     }
 })
+
+router.delete("/deleteUser/:id", async (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    try {
+        const deleteUser = await User.findOneAndDelete({_id : id});
+        console.log("User deleted successfully");
+        res.status(200).json(deleteUser);
+    } catch (error) {
+        console.log("Error in fetching user", error);
+
+    }
+})
+router.patch("/updateUser/:id", async (req, res) => {
+    const { id } = req.params;
+    const {name, email, age, gender} = req.body;
+
+    console.log(id);
+    try {
+        const updateUser = await User.findOneAndDelete(id, req.body, {new: true});
+        console.log("User Updated successfully");
+        res.status(200).json(updateUser);
+    } catch (error) {
+        console.log("Error in fetching user", error);
+
+    }
+})
+
+
 
 export default router;
